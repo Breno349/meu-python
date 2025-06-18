1d4831ce500258d4fd99791b3a4b1ecc6b41266f
Breno349 <123127632+Breno349@users.noreply.github.com>
2025-06-18 18:29:02 -0300

    Update index.py
    jaja

--- api/index.py
+++ api/index.py
@@ -1,13 +1,14 @@
-"""from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler
  
 class handler(BaseHTTPRequestHandler):
  
-    def do_GET(self):
+"""def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
         return"""
+
 def handler(request, response):
     # Cabeçalhos customizados
     response.set_header('Content-Type', 'text/plain; charset=utf-8')
